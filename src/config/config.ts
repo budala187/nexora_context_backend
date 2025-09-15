b79414ef42f5406848a1b413b68f46cf9f5dcd05
budala187 <kojatobudalapucanamene@gmail.com>
2025-09-15 16:37:15 +0200

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+  serverName: 'Greeting MCP',
+  serverVersion: '1.0.0',
+  port: process.env.PORT || Number(3002),
+  skEnvUrl: process.env.SK_ENV_URL || '',
+  skClientId: process.env.SK_CLIENT_ID || '',
+  skClientSecret: process.env.SK_CLIENT_SECRET || '',
+  logLevel: 'info',
+  mcpServerId: process.env.MCP_SERVER_ID || '',
+  protectedResourceMetadata: process.env.PROTECTED_RESOURCE_METADATA || '',
+  expectedAudience: process.env.EXPECTED_AUDIENCE || '',
+};
